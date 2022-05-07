9a2ebdc0642b499c474f99f60a3ee764310845f2
Ca1eb06 <104589136+Ca1eb06@users.noreply.github.com>
2022-05-07 11:47:04 -0700

    Create ok.js

---
+++ ok.js
@@ -0,0 +1,15 @@
+var questionOne = prompt("What is your nickname?");
+ document.getElementById("blankOne").innerHTML = questionOne;
+
+ var questionOne = prompt(" Type your best friend's name here ");
+ document.getElementById("blankTwo").innerHTML = questionOne;
+
+ var questionOne = prompt(" Please enter your favorite phrase ");
+ document.getElementById("blankThree").innerHTML = questionOne + '!!' ;
+
+
+ var questionOne = prompt(" Enter any random noun thank! ");
+ document.getElementById("blankFour").innerHTML = questionOne ;
+
+
+ 
